a6326c6cde95daa1890b4bdde834586687f9bd20
Alexander Sagen <alexmsagen@gmail.com>
2017-08-09 15:18:49 +0200

    Added effects when placing a multistory elevator

--- src/org/pzyko/ironelevator/ActionHelper.java
+++ src/org/pzyko/ironelevator/ActionHelper.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
@@ -19,7 +20,7 @@ public class ActionHelper {
 		PacketPlayOutChat packetPlayOutChat = new PacketPlayOutChat(barmsg, ChatMessageType.GAME_INFO);
 		((CraftPlayer) p).getHandle().playerConnection.sendPacket(packetPlayOutChat);
 	}
-	
+
 	public static void playoutElevatorEffect(Player p, Location from, Location to) {
 		p.getWorld().playSound(from, Sound.ENTITY_IRONGOLEM_ATTACK, 1.0F, 0.0F);
 		p.getWorld().playSound(to, Sound.ENTITY_IRONGOLEM_ATTACK, 1.0F, 0.0F);
@@ -35,11 +36,11 @@ public class ActionHelper {
 
 				int particles = 50;
 				float radius = 0.3f;
-				
+
 				Location location1 = p.getEyeLocation().clone();
 				Location location2 = p.getEyeLocation().clone();
 				Location location3 = p.getEyeLocation().clone();
-				
+
 				for (int par = 0; par < particles; par++) {
 					double angle, x, z;
 					angle = 2 * Math.PI * par / particles;
@@ -55,12 +56,12 @@ public class ActionHelper {
 					location2.subtract(x, -0.66, z);
 					location3.subtract(x, -1.33, z);
 				}
-				
+
 				from.add(0, 1.62, 0); // Eye position
 				location1 = from.clone();
 				location2 = from.clone();
 				location3 = from.clone();
-				
+
 				for (int par = 0; par < particles; par++) {
 					double angle, x, z;
 					angle = 2 * Math.PI * par / particles;
@@ -81,36 +82,39 @@ public class ActionHelper {
 			e1.printStackTrace();
 		}
 	}
-	
+
 	public static void playoutPlaceElevatorEffect(Player p, Block b) {
-//		p.getWorld().playSound(l, Sound.BLOCK_END_PORTAL_FRAME_FILL, 1.0F, 0.0F);
-//
-//		try {
-//			for(Player o : Bukkit.getOnlinePlayers()) {
-//
-//				if(o.getWorld() != p.getWorld()) continue;
-//				if(o.getLocation().distance(p.getLocation()) > 50) continue;
-//				
-//				ParticleEffect.ANGRY_VILLAGER.sendToPlayer(o, l, 0, 0, 0, 0, 10);
-//
-//				l.add(0, 3f, 0);
-//
-//				Location location = l;
-//				int particles = 50;
-//				float radius = 0.3f;
-//				for (int par = 0; par < particles; par++) {
-//					double angle, x, z;
-//					angle = 2 * Math.PI * par / particles;
-//					x = Math.cos(angle) * radius;
-//					z = Math.sin(angle) * radius;
-//					location.add(x, -1.6, z);
-//					ParticleEffect.HAPPY_VILLAGER.sendToPlayer(o, location, 0, 0, 0, 0, 1);
-//					location.subtract(x, -1.6, z);
-//				}
-//			}
-//		} catch (Exception e1) {
-//			e1.printStackTrace();
-//		}
+		Location l = b.getRelative(BlockFace.UP).getLocation();
+		l.add(.5, 0, .5);
+		
+		p.getWorld().playSound(l, Sound.BLOCK_END_PORTAL_FRAME_FILL, 1.0F, 0.0F);
+		
+		l.add(0, 0, 0);
+		
+		try {
+			for(Player o : Bukkit.getOnlinePlayers()) {
+
+				if(o.getWorld() != p.getWorld()) continue;
+				if(o.getLocation().distance(p.getLocation()) > 50) continue;
+
+				ParticleEffect.ANGRY_VILLAGER.sendToPlayer(o, l, 0, 0, 0, 0, 10);
+
+				Location location = l.clone();
+				int particles = 50;
+				float radius = 0.3f;
+				for (int par = 0; par < particles; par++) {
+					double angle, x, z;
+					angle = 2 * Math.PI * par / particles;
+					x = Math.cos(angle) * radius;
+					z = Math.sin(angle) * radius;
+					location.add(x, .2, z);
+					ParticleEffect.HAPPY_VILLAGER.sendToPlayer(o, location, 0, 0, 0, 0, 1);
+					location.subtract(x, .2, z);
+				}
+			}
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
 	}
-	
+
 }
--- src/org/pzyko/ironelevator/ElevatorListener.java
+++ src/org/pzyko/ironelevator/ElevatorListener.java
@@ -1,5 +1,6 @@
 package org.pzyko.ironelevator;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.bukkit.Location;
@@ -22,22 +23,22 @@ public class ElevatorListener implements Listener {
 		if(!p.hasPermission("pzyko.action.ironelevator")) return;
 		if(p.isSneaking()) return;
 		if(p.isFlying()) return;
-		
+
 		Optional<ElevatorBlock> currentElevator = ElevatorUtils.getElevator(b);
 		if(!currentElevator.isPresent()) return;
-		
+
 		Optional<ElevatorBlock> nextElevator = ElevatorUtils.getNextElevator(b, false);
 		if(!nextElevator.isPresent()) return;
 
 		Block nextElevatorBlock = nextElevator.get().block;
 		Elevator elevator = nextElevator.get().elevator;
-		
+
 		Location l = elevator.getTeleportLocation(nextElevatorBlock);
 		l.setYaw(p.getLocation().getYaw());
 		l.setPitch(p.getLocation().getPitch());
 
 		if(!ElevatorPlugin.INSTANCE.gph.playerCanAccessBlock(p, b)) return;
-		
+
 		p.teleport(l);
 		p.setVelocity(new Vector(0, 0, 0));
 		p.setFlying(false);
@@ -53,41 +54,71 @@ public class ElevatorListener implements Listener {
 		if(p.isSneaking()) return; // Hindre heisen i å virke ved sneak (snike og hoppe)
 		if(p.isFlying()) return;
 		if(e.getFrom().getY() >= e.getTo().getY()) return;
-		
+
 		// Fix for halfblocks
 		if((e.getFrom().getY() - ((int)e.getFrom().getY()) < .5) 
 				&& !p.getLocation().getBlock().getRelative(BlockFace.UP, 2).getType().isSolid()) return;
 
 		Optional<ElevatorBlock> currentElevator = ElevatorUtils.getElevator(b);
 		if(!currentElevator.isPresent()) return;
-		
+
 		Optional<ElevatorBlock> nextElevator = ElevatorUtils.getNextElevator(b, true);
 		if(!nextElevator.isPresent()) return;
 
 		Block nextElevatorBlock = nextElevator.get().block;
 		Elevator elevator = nextElevator.get().elevator;
-		
+
 		Location l = elevator.getTeleportLocation(nextElevatorBlock);
 		l.setYaw(p.getLocation().getYaw());
 		l.setPitch(p.getLocation().getPitch());
-		
+
 		if(!ElevatorPlugin.INSTANCE.gph.playerCanAccessBlock(p, b)) return;
-		
+
 		p.teleport(l);
 		p.setVelocity(new Vector(0, 0, 0));
 		p.setFlying(false);
 		ActionHelper.sendActionMessage(p, "§aDu tok heisen OPP");
 		ActionHelper.playoutElevatorEffect(p, currentElevator.get().elevator.getTeleportLocation(b), l);
 	}
-	
+
 	@EventHandler
 	public void elevatorPlace(BlockPlaceEvent e) {
-//		Player p = e.getPlayer();
-//		Block b = e.getBlockPlaced();
-//		
-//		Optional<ElevatorBlock> elevatorBlock = Optional.empty();
-//		if(!(elevatorBlock = ElevatorUtils.isPartOfElevator(b)).isPresent()) return;
-//		
-//		// TODO:
+		Player p = e.getPlayer();
+		Block b = e.getBlockPlaced();
+
+		Optional<ElevatorBlock> elevatorBlock = Optional.empty();
+		if(!(elevatorBlock = ElevatorUtils.isPartOfElevator(b)).isPresent()) return;
+		if(!elevatorBlock.get().elevator.isSafeTeleportTo(elevatorBlock.get().elevator.getTeleportLocation(elevatorBlock.get().block))) return;
+
+		ArrayList<Block> storiesFound = new ArrayList<Block>();
+		storiesFound.add(elevatorBlock.get().block);
+		
+		Block lastElevator = elevatorBlock.get().block;
+		while(lastElevator != null) {
+			Optional<ElevatorBlock> ele = ElevatorUtils.getNextElevator(lastElevator, true);
+			if(ele.isPresent()) {
+				storiesFound.add(ele.get().block);
+				lastElevator = ele.get().block;
+			} else {
+				lastElevator = null;
+			}
+		}
+
+		lastElevator = elevatorBlock.get().block;
+		while(lastElevator != null) {
+			Optional<ElevatorBlock> ele = ElevatorUtils.getNextElevator(lastElevator, false);
+			if(ele.isPresent()) {
+				storiesFound.add(ele.get().block);
+				lastElevator = ele.get().block;
+			} else {
+				lastElevator = null;
+			}
+		}
+
+		if(storiesFound.size() > 1) {
+			for(Block elevatorStory : storiesFound)
+				ActionHelper.playoutPlaceElevatorEffect(p, elevatorStory);
+			ActionHelper.sendActionMessage(p, "§aDu laget en heis med " + storiesFound.size() + " etasjer!");
+		}
 	}
 }
